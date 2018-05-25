2ba3f6e42dde2380132cda7fb077f7196947214c
Sean Feldman <feldman.sean@gmail.com>
2018-05-25 16:45:59 -0600

    Benchmark creation and disposal of MessageSenders

---
+++ MessageSenderBenchmarking/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MessageSenderBenchmarking
+{
+    using System.Threading.Tasks;
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Attributes.Jobs;
+    using BenchmarkDotNet.Running;
+    using Microsoft.Azure.ServiceBus;
+    using Microsoft.Azure.ServiceBus.Core;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MessageSenderBenchmarks>();
+        }
+    }
+
+    [CoreJob, ClrJob]
+    [MemoryDiagnoser]
+    public class MessageSenderBenchmarks
+    {
+        private ServiceBusConnection connection;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus.ConnectionString", EnvironmentVariableTarget.Machine);
+            connection = new ServiceBusConnection(connectionString);
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            connection.CloseAsync().GetAwaiter().GetResult();
+        }
+
+
+        [Benchmark]
+        public async Task CreateAndDisposeMessageSender()
+        {
+            var sender = new MessageSender(connection, "queue-0");
+            await sender.CloseAsync().ConfigureAwait(false);
+        }
+    }
+}
